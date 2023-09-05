85754da1a11122eaa7c1188c90745b97fa028a47
Alva A <rpondering@gmail.com>
2023-09-05 21:40:04 +0700

    feat: persist gql result

--- src/component/ContactList/index.tsx
+++ src/component/ContactList/index.tsx
@@ -19,7 +19,7 @@ export const Contact: React.FC<{ contact: IContact }> = ({ contact }) => {
 }
 
 export const ContactList: React.FC = () => {
-  const { loading, data, fetchMore, variables } = useQuery<
+  const { loading, data, variables, refetch, networkStatus } = useQuery<
     GetContactList,
     GetContactListVariables
   >(GET_CONTACT_LIST, {
@@ -28,29 +28,45 @@ export const ContactList: React.FC = () => {
         last_name: 'asc',
       },
     },
+    fetchPolicy: 'cache-only',
+    notifyOnNetworkStatusChange: true,
   })
 
   const [offset, setOffset] = useState(0)
 
-  useEffect(() => {
-    fetchMore({
-      variables: {
-        offset: offset,
-      },
-    })
-  }, [fetchMore, offset])
+  // TODO: offsetMore with cache, it's currently not fetching from cache
+  // const [offsetMore, setOffsetMore] = useState(0)
+  // useEffect(() => {
+  //   fetchMore({
+  //     variables: {
+  //       offset: offsetMore,
+  //     },
+  //   })
+  // }, [fetchMore, offsetMore])
 
-  if (loading) return <>Loading...</>
+  useEffect(() => {
+    refetch({
+      offset: offset,
+    })
+  }, [offset, refetch])
+
   return (
     <>
+      {loading ? <>Loading...</> : <>Ready</>}
+      {networkStatus}
       <button
         onClick={() => {
           if (typeof variables?.offset === 'undefined') return
-          setOffset((prev) => prev + 10)
+          // setOffsetMore((prev) => prev + 10)
         }}
       >
-        Refetch
+        Fetch More
       </button>
+
+      <button onClick={() => setOffset((prev) => (prev === 0 ? 0 : prev - 10))}>
+        prev
+      </button>
+      <button onClick={() => setOffset((prev) => prev + 10)}>Next</button>
       {data?.contact?.map((contact) => {
         return <Contact key={contact.id} contact={contact} />
       })}
--- src/gql/client.ts
+++ src/gql/client.ts
@@ -1,31 +1,38 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { Contact } from './contact/type'
+import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist'
+
+const cache = new InMemoryCache({})
+// await before instantiating ApolloClient, else queries might run before the cache is persisted
+await persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage),
+})
 
 export const client = new ApolloClient({
   uri: process.env.REACT_APP_GQL_URL,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          contact: {
-            keyArgs: [],
-            // merge(existing = [], incoming = []) {
-            //   return [...existing, ...incoming]
-            // },
-            merge(
-              existing?: { __ref: string }[],
-              incoming?: { __ref: string }[]
-            ) {
-              const idSet = new Set(existing?.map((ext) => ext.__ref) ?? [])
-              incoming?.forEach((inc) => {
-                idSet.add(inc.__ref)
-              })
-
-              return Array.from(idSet.values()).map((uniq) => ({ __ref: uniq }))
-            },
-          },
-        },
-      },
-    },
-  }),
+  cache,
+  // cache: new InMemoryCache({
+  // typePolicies: {
+  //   Query: {
+  //     fields: {
+  //       contact: {
+  //         keyArgs: [],
+  //         // merge(existing = [], incoming = []) {
+  //         //   return [...existing, ...incoming]
+  //         // },
+  //         merge(
+  //           existing?: { __ref: string }[],
+  //           incoming?: { __ref: string }[]
+  //         ) {
+  //           const idSet = new Set(existing?.map((ext) => ext.__ref) ?? [])
+  //           incoming?.forEach((inc) => {
+  //             idSet.add(inc.__ref)
+  //           })
+  //           return Array.from(idSet.values()).map((uniq) => ({ __ref: uniq }))
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
+  // }),
 })
