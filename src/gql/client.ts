d12e80218d02d37033564eb1635b7928d57fb5e1
Alva A <rpondering@gmail.com>
2023-09-05 19:07:16 +0700

    fix: double quotation bug in netlify

--- src/gql/client.ts
+++ src/gql/client.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
+const reactAppGqlUrl = () => {
+  return process.env.REACT_APP_GQL_URL?.replace(/"/g, '')
+}
+
 const link = new HttpLink({
-  uri: process.env.REACT_APP_GQL_URL,
+  uri: reactAppGqlUrl(),
 })
 
 export const client = new ApolloClient({
