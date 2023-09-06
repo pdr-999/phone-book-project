e2bbdc0313042cbbdcc1be145c4ce9d0f634c517
Alva A <rpondering@gmail.com>
2023-09-06 14:58:31 +0700

    docs: todo

--- src/pages/contact/create.tsx
+++ src/pages/contact/create.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 export const CreateContact: React.FC = () => {
+  // TODO: make navigation not go back and forth in PWA
   return (
     <>
       <h4> Welcome to create contact page </h4>
