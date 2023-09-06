8229f0c86cfe980a22e45e06797c7c137f07f672
Alva A <rpondering@gmail.com>
2023-09-06 22:17:34 +0700

    feat: base color

--- src/index.tsx
+++ src/index.tsx
@@ -27,7 +27,43 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <MantineProvider emotionCache={myCache} withGlobalStyles withNormalizeCSS>
+      <MantineProvider
+        emotionCache={myCache}
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+          // https://omatsuri.app/color-shades-generator
+          colors: {
+            // Darken 18% Saturation -10%, base: #00aa5b
+            green: [
+              '#FFF',
+              '#FFF',
+              '#D8F3E6',
+              '#9FE6C5',
+              '#6AE0A9',
+              '#38E092',
+              '#16D67D',
+              '#0ABF6B',
+              '#00AA5B',
+              '#07844A',
+            ],
+            // Darken 10% Saturation -10%, base: #1b2632
+            dark: [
+              '#AFB9C5',
+              '#7B8EA3',
+              '#586D84',
+              '#425467',
+              '#314051',
+              '#253140',
+              '#1B2632',
+              '#141B22',
+              '#0E1317',
+              '#0A0D10',
+            ],
+          },
+        }}
+      >
         <RouterProvider router={router} />
       </MantineProvider>
     </ApolloProvider>
